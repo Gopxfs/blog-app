08977b058bd9f72f569e12feb87be7941c5e2602
Gabriel <gopxfs@hotmail.com>
2022-09-13 20:36:48 -0300

    Fix migrations

--- db/migrate/20220913182323_add_user_ref_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :posts, :users, null: false, foreign_key: true
-  end
-end
--- db/migrate/20220913233303_create_posts.rb
+++ db/migrate/20220913233303_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :author_id
       t.string :title
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
