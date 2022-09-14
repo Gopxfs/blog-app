5e3fac6bdbaba3cf124b55127a658f4ebccabdc2
Gabriel <gopxfs@hotmail.com>
2022-09-13 21:14:15 -0300

    Add methods to update posts counter & show recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
 
   belongs_to :user
+
+  def self.update_posts_counter(user)
+    user.posts_counter = user.posts.length
+  end
+
+  def self.show_recent_comments(post)
+    post.comments.order('created_at DESC').limit(5)
+  end
 end
