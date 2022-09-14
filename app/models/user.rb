1c278d82f3c25fd98e17dd466df488858a952d17
Gabriel <gopxfs@hotmail.com>
2022-09-14 20:48:20 -0300

    Update has_many to have a foreign key and destroy dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: :user_id, dependent: :destroy
+  has_many :comments, foreign_key: :user_id, dependent: :destroy
+  has_many :likes, foreign_key: :user_id, dependent: :destroy
 
   def show_recent_posts
     posts.order('created_at DESC').limit(3)
