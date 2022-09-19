e89f6c286788bf326459d49bc27d5639f0beca05
Gabriel <gopxfs@hotmail.com>
2022-09-19 14:37:27 -0300

    Update rotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # --- GET ---
+  # users
   get "users", to: "users#index"
+  get "users/:id", to: "users#show"
+  # users posts
+  get "users/:user_id/posts", to: "posts#index"
+  get "users/:user_id/posts/:id", to: "posts#show"
 
   root to: "users#index"
 end
