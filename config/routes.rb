45eb08a8fc2235c4322fa84fffc087b512cc31fa
Gabriel <gopxfs@hotmail.com>
2022-09-19 20:21:40 -0300

    Update routes to nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # --- GET ---
   # users
-  get "users", to: "users#index"
-  get "users/:id", to: "users#show"
-  # users posts
-  get "users/:user_id/posts", to: "posts#index"
-  get "users/:user_id/posts/:id", to: "posts#show"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
   root to: "users#index"
 end
