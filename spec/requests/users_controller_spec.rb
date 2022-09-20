0db01e7e1f7014b6b16d9c886c76840595f77387
Gabriel <gopxfs@hotmail.com>
2022-09-20 13:45:54 -0300

    Add testing to users controller

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Users Controller", :type => :request do
-    it "renders and redirects to the User's page" do
+  describe "GET #index" do
+    it "renders the Users page" do
       get "/users"
+
+      expect(response).to be_successful
+
       expect(response).to render_template(:index)
+
+      expect(response.body).to include("Users list")
     end
+  end
+
+  describe 'GET #show' do
+    it "renders the User page" do
+      get '/users/1'
+
+      expect(response).to be_successful
+
+      expect(response).to render_template(:show)
+
+      expect(response.body).to include("User")
+    end
+  end
 end
