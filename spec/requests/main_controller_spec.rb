61dc1fa6251cf3f7f456c9ca141db950a5a3ef39
Gabriel <gopxfs@hotmail.com>
2022-09-20 14:10:30 -0300

    Add tests to main route

--- spec/requests/main_controller_spec.rb
+++ spec/requests/main_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Main Controller', type: :request do
+  describe 'GET #index' do
+    it 'renders the main page' do
+      get '/main'
+
+      expect(response).to be_successful
+
+      expect(response).to render_template(:index)
+
+      expect(response.body).to include('Main')
+    end
+  end
 end
