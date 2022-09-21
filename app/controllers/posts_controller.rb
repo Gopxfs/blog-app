742be5c34700a3eba6444ec0515b2ce442dfffcd
Gabriel <gopxfs@hotmail.com>
2022-09-21 13:44:38 -0300

    Add parameters to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comments.find(params[:post_id])
+  end
 end
