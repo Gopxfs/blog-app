bc8a346cef2bf4251791208fcc1bb0d57a9c9007
Gabriel <gopxfs@hotmail.com>
2022-09-21 11:44:39 -0300

    Update counters method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,6 @@ class Comment < ApplicationRecord
   private
 
   def update_comments_counter
-    post.update(comments_counter: post.comments.length)
+    post.increment!(:comments_counter)
   end
 end
