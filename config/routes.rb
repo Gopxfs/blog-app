46f53aca7b1266a7650f1fcc240c564e874e9465
Gabriel <gopxfs@hotmail.com>
2022-09-22 19:15:17 -0300

    Add create route to comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # --- GET ---
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+    end
   end
 
   resources :main, only: [:index]
 
   root to: "users#index"
-
-  # --- Post ---
-  post 'create_comment', to: 'comment#create'
 end
