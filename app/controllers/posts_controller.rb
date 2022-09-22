8264f99567148c5221598f5f5e46e42e8fec00f2
Gabriel <gopxfs@hotmail.com>
2022-09-22 18:04:46 -0300

    Add new & create methods to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,31 @@ class PostsController < ApplicationController
     @comments = @post.comments
   end
 
-  def new; end
+  def new
+    @post = Post.new
+    @user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post }}
+    end
+  end
 
-  def create; end
+  def create
+    author = current_user
+    post = Post.new(params.require(:post).permit(:comments_counter, :likes_counter, :author, :title, :text))
+    post.likes_counter = 0
+    post.comments_counter = 0
+    post.author = author
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post created successfully!'
+          redirect_to user_url(author)
+        else
+          flash.now[:error] = "Error: Post could not be created."
+          render :new, locals: { post: post }, status: 422
+        end
+      end
+    end
+  end
 end
