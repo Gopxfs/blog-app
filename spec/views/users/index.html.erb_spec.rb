d1d4c2a86f9ac16ca321849a38b1f21dc2a46256
Gabriel <gopxfs@hotmail.com>
2022-09-26 16:27:39 -0300

    Add test cases to users #index

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,17 +1,33 @@
-describe 'the users list', type: :feature do
+require 'rails_helper'
+
+RSpec.describe 'users #index', type: :feature do
   before :each do
-    @users = User.all
+    user1 = User.create(
+      name: 'Gabriel',
+      photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
+      bio: "I'm a student at Microverse",
+      posts_counter: 0
+    )
+    user2 = User.create(
+        name: 'Second User',
+        photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
+        bio: "I'm not a student at Microverse",
+        posts_counter: 0
+      )
+    @users = [user1, user2]
+    visit '/users'
   end
 
   it 'displays all users cards' do
-    visit '/users'
-    within ('#users') do
-      @users.each do |user|
-        expect(page).to have_content user.name
-        expect(page).to have_content user.photo
-        expect(page).to have_content user.posts_counter
-      end
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+      expect(page).to have_css("img[src*='#{user.photo}']")
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
     end
   end
 
+  it 'changes to user profile after clicking on a card' do
+    click_link("#{@users.first.name}")
+    expect(page).to have_content("Profile")
+  end
 end
