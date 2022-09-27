6f4bf69226e8b2ddf41f936d2e250352187b44a7
Gabriel <gopxfs@hotmail.com>
2022-09-27 16:37:16 -0300

    Update validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |params|
-      params.permit(:name, :email, :password, :password_confirmation)
-    end
+    default_parameters = [:name, :email, :encrypted_password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: default_parameters)
+    devise_parameter_sanitizer.permit(:sign_in, keys: default_parameters)
+    devise_parameter_sanitizer.permit(:account_update, keys: default_parameters)
   end
 end
