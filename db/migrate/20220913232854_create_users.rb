ef1b95c6b5a5ce16af20d32f69b6652fe1d834ae
Gabriel <gopxfs@hotmail.com>
2022-09-27 16:54:52 -0300

    Update default values

--- db/migrate/20220913232854_create_users.rb
+++ db/migrate/20220913232854_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name :default: "New User"
-      t.string :photo  :default: "https://i.imgur.com/8Q5ZQ2x.png"
-      t.text :bio :default => "I'm a new user"
+      t.string :name
+      t.string :photo,  default: "https://i.imgur.com/8Q5ZQ2x.png"
+      t.text :bio, default: "I'm a new user"
       t.integer :posts_counter, default: 0
 
       t.timestamps
