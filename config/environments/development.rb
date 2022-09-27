670414dceec01f0aad7cd50fc8e55ca8278c8b84
Gabriel <gopxfs@hotmail.com>
2022-09-27 15:02:25 -0300

    Add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = { 
+    user_name: ENV['SENDMAIL'],
+    password: ENV['SENDMAIL_PASSWORD'],
+    domain: ENV['MAIL_HOST'],
+    address: 'smtp@gmail.com',
+    port: '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
