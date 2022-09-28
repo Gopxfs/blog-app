feadf704fff4ac7232260e59148d9c5e7710b91d
Gabriel <gopxfs@hotmail.com>
2022-09-28 14:12:51 -0300

    Add method to update posts counter after deleting post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,8 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_posts_counter
+  after_save :increment_posts_counter
+  after_destroy :decrement_posts_counter
 
   def show_recent_comments
     comments.order('created_at DESC').limit(5)
@@ -16,7 +17,11 @@ class Post < ApplicationRecord
 
   private
 
-  def update_posts_counter
+  def increment_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
