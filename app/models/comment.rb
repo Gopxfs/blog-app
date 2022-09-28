be1b4cd7744668eb57d5662cc5d0d9764126b0bd
Gabriel <gopxfs@hotmail.com>
2022-09-28 14:18:57 -0300

    Add method to update comments after deleting a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,7 @@ class Comment < ApplicationRecord
   private
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.comments_counter = post.comments.length
+    post.save
   end
 end
