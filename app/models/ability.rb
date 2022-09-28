06cc0e230151b04b41ccab37acccb13224678ae4
Gabriel <gopxfs@hotmail.com>
2022-09-28 15:14:22 -0300

    Remove return condition inside initialize

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return if user.nil?
-
-    return can :manage, :all if user.role == 'admin'
+    can :manage, :all if user.role == 'admin'
 
     can :delete_post, Post, author: user
     can :delete_comment, Comment, author: user
