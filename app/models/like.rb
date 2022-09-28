caa68c4271ac2de3ea86c669aa7511095477843b
Gabriel <gopxfs@hotmail.com>
2022-09-28 14:18:30 -0300

    Add method to update likes after deleting the like

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,12 +3,14 @@ class Like < ApplicationRecord
   belongs_to :post
 
   after_save :update_likes_counter
+  after_destroy :update_likes_counter
 
   validates :author, uniqueness: { scope: :post }
 
   private
 
   def update_likes_counter
-    post.increment!(:likes_counter)
+    post.likes_counter = post.likes.length
+    post.save
   end
 end
