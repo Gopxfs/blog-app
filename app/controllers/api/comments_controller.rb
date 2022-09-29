452482f16c4afd725adc633ce9fe5994a0372cec
Gabriel <gopxfs@hotmail.com>
2022-09-29 14:43:16 -0300

    Add comment parameters to api controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,4 +7,12 @@ class Api::CommentsController < ApplicationController
       format.json  { render :json => post.comments }
     end
   end
+
+  def create
+    comment = Comment.new(params.require(:comment).permit(:text))
+    comment.author = current_user
+    comment.post = Post.find(params[:post_id])
+
+    render json: comment, status: :created if comment.save
+  end
 end
