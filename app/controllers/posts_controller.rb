f138b31aee4c5998f3d34b55c58c159bad6c2616
Gabriel <gopxfs@hotmail.com>
2022-09-29 08:55:35 -0300

    Add endpoint to list all posts for a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+
+    respond_to do |format|
+      format.json { render :json => @posts }
+    end
   end
 
   def show
